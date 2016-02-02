15258cf84176b5dc96a9b16d1d94079d74c84787
yigitcan bacakoglu <yigitcan_bacakoglu@hotmail.com>
2016-02-02 17:53:27 +0200

    open

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use( '/open',      routes.open );
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/open', function(req, res, next) {
+
+	var gpio = require("pi-gpio");
+
+	var intervalId;
+	var durationId;
+	var gpioPin = 16;    // header pin 16 = GPIO port 23
+
+	gpio.open(gpioPin, "output", function(err) {
+	        gpio.write(gpioPin, 1, function() {});
+	});
+
+	durationId = setTimeout( function(){
+	  clearTimeout(durationId);
+	  gpio.write(gpioPin, 0, function() { // turn off pin 16
+	  gpio.close(gpioPin); // then Close pin 16
+	  process.exit(0); // and terminate the program
+	  });
+	}, 200); // duration in mS
+	
+  res.send('respond with a resource');
+});
+
+
 module.exports = router;
